067903a559d08d89154d9c835047fb92883f1cd9
liukxaa <644306737@qq.com>
2018-03-31 21:19:54 +0800

    完成 collect_all_even

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,6 +2,13 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var even_collection =  [];
+  for (var i = 0; i < collection.length; i++){
+    if (collection[i] % 2 === 0) {
+      even_collection.push(collection[i]);
+    }
+  }
+  return even_collection;
 }
 
 module.exports = collect_all_even;
