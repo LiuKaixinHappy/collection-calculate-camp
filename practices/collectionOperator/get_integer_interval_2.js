529a9d323475f262b88b0494b638add7a8d9b022
liukxaa <644306737@qq.com>
2018-03-31 21:46:18 +0800

    完成 get_integer_interval_2

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,36 @@
 'use strict';
 
+function is_even(number) {
+  return number % 2 === 0;
+}
+
+function get_decrease_even_interval(number_a, number_b) {
+  var interval = [];
+  for (var i = number_a; i >= number_b; i--) {
+    if (is_even(i)) {
+      interval.push(i);
+    }
+  }
+  return interval;
+}
+
+function get_increase_even_interval(number_a, number_b) {
+  var interval = [];
+  for (var i = number_a; i <= number_b; i++) {
+    if (is_even(i)) {
+      interval.push(i);
+    }
+  }
+  return interval;
+}
+
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
+  if (number_a > number_b) {
+    return get_decrease_even_interval(number_a, number_b);
+  } else {
+    return get_increase_even_interval(number_a, number_b);
+  }
 }
 
 module.exports = get_integer_interval_2;
