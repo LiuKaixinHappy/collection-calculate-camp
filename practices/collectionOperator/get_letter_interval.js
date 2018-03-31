366c5a9462dbcbbed50507766707a7d058dd8fcd
liukxaa <644306737@qq.com>
2018-03-31 22:27:40 +0800

    完成 get_letter_interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,28 @@
 'use strict';
 
+function get_decrease_interval(number_a, number_b) {
+  var interval = [];
+  for (var i = number_a; i >= number_b; i--) {
+    interval.push(String.fromCharCode(96 + i));
+  }
+  return interval;
+}
+
+function get_increase_interval(number_a, number_b) {
+  var interval = [];
+  for (var i = number_a; i <= number_b; i++) {
+    interval.push(String.fromCharCode(96 + i));
+  }
+  return interval;
+}
+
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  if (number_a > number_b) {
+    return get_decrease_interval(number_a, number_b);
+  } else {
+    return get_increase_interval(number_a, number_b);
+  }
 }
 
 module.exports = get_letter_interval;
