2a776f0e8577098a1fdb3b747eb95a72d4921d0a
liukxaa <644306737@qq.com>
2018-04-04 13:34:49 +0800

    完成 get_letter_interval_2

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,7 +1,36 @@
 'use strict';
 
+function get_decrease_interval(number_a, number_b) {
+  var interval = [];
+  for (var j = number_a; j >= number_b; j--) {
+    if (j <= 26) {
+      interval.push(String.fromCharCode(96 + j));
+    } else {
+      interval.push(String.fromCharCode(96 + (j - 1) / 26) + String.fromCharCode(97 + (j - 1) % 26));
+    }
+  }
+  return interval;
+}
+
+function get_increase_interval(number_a, number_b) {
+  var interval = [];
+  for (var j = number_a; j <= number_b; j++) {
+    if (j <= 26) {
+      interval.push(String.fromCharCode(96 + j));
+    } else {
+      interval.push(String.fromCharCode(96 + (j - 1) / 26) + String.fromCharCode(97 + (j - 1) % 26));
+    }
+  }
+  return interval;
+}
+
 function get_letter_interval_2(number_a, number_b) {
   //在这里写入代码
+  if (number_a > number_b) {
+    return get_decrease_interval(number_a, number_b);
+  } else {
+    return get_increase_interval(number_a, number_b);
+  }
 }
 
 module.exports = get_letter_interval_2;
