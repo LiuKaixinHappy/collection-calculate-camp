e8edb4bcf856300a4a4542e5f9e7c28eb19d46e6
liukxaa <644306737@qq.com>
2018-04-07 22:26:54 +0800

    完成 grouping_count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,21 @@
 'use strict';
 
 function grouping_count(collection) {
+  let sort_collection = collection.sort();
 
-  //在这里写入代码
+  let unique_index = sort_collection.filter((element, index, array) => {
+    return array.indexOf(element) === index;
+  });
+
+  let result = {};
+  for (let i = 0; i < unique_index.length - 1; i++) {
+    result[unique_index[i]] = sort_collection.indexOf(unique_index[i + 1])
+      - sort_collection.indexOf(unique_index[i]);
+  }
+  result[unique_index[unique_index.length - 1]] =
+    sort_collection.length - sort_collection.indexOf(unique_index[unique_index.length - 1]);
+
+  return result;
 }
 
 module.exports = grouping_count;
