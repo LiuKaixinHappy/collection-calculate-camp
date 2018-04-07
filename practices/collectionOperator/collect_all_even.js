66b0420a37f8f630afe7e6be910d30979fcdd18c
liukxaa <644306737@qq.com>
2018-04-07 15:02:19 +0800

    完成 choose_even

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -3,9 +3,9 @@
 function collect_all_even(collection) {
   //在这里写入代码
   let even_collection =  [];
-  for (let i = 0; i < collection.length; i++){
-    if (collection[i] % 2 === 0) {
-      even_collection.push(collection[i]);
+  for (let elem of collection){
+    if (elem % 2 === 0) {
+      even_collection.push(elem);
     }
   }
   return even_collection;
--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_even(collection) {
-
   //在这里写入代码
+  let even_collection =  [];
+  for (let elem of collection){
+    if (elem % 2 === 0) {
+      even_collection.push(elem);
+    }
+  }
+  return even_collection;
 }
 
 module.exports = choose_even;
