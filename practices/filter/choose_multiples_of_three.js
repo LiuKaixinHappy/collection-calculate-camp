09015a342c0996258a8e65c07b9550c152565360
liukxaa <644306737@qq.com>
2018-04-07 15:04:51 +0800

    完成 choose_multiples_of_three

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
   //在这里写入代码
+  let result = [];
+  for (let elem of collection) {
+    if (elem % 3 === 0) {
+      result.push(elem)
+    }
+  }
+  return result;
 }
 
 module.exports = choose_multiples_of_three;
