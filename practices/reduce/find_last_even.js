a8d07e3e3cb930007fe40031d45140156d12d86e
liukxaa <644306737@qq.com>
2018-04-08 14:48:04 +0800

    完成 split_to_zeros

--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function find_last_even(collection) {
-  for (let i = collection[length - 1]; i >= 0; i--) {
+  for (let i = collection[collection.length - 1]; i >= 0; i--) {
     if (collection[i] % 2 === 0) {
       return collection[i];
     }
--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  let result = [];
+  for (let i = number; i > 0; i -= interval) {
+    result.push(parseFloat(i.toFixed(1)));
+  }
+  let last_elem = result[result.length - 1];
+  if (last_elem !== 0) {
+    result.push(parseFloat((last_elem - interval).toFixed(1)));
+  }
+  return result;
 }
 
 module.exports = spilt_to_zero;
