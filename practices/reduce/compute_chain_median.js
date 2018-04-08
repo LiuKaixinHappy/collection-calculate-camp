6a570eb4e9f61de184d20956a7bb8eb6311d9068
liukxaa <644306737@qq.com>
2018-04-08 14:06:39 +0800

    完成 compute_chain_median

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  let array = collection.split('->').map(elem => parseInt(elem, 10)).sort((x, y) => {return x - y;});
+  return array.length % 2 === 0 ? (array[array.length / 2] + array[array.length / 2] - 1) / 2 :
+    (array[(array.length - 1) / 2]);
 }
 
 module.exports = compute_chain_median;
