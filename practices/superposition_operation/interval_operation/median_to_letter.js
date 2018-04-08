ec2c8db8255006f9aad1bc934453c7d659f71b36
liukxaa <644306737@qq.com>
2018-04-08 17:05:00 +0800

    完成 interval_operation的median_to_letter

--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function median_to_letter(collection) {
-
-  //在这里写入代码
+  let median;
+  if (collection.length % 2 === 0) {
+    median = Math.ceil((collection[collection.length / 2] + collection[collection.length / 2 - 1]) / 2);
+  } else {
+    median = Math.ceil(collection[(collection.length - 1) / 2] / 2);
+  }
+  if (median <= 26) {
+    return String.fromCharCode(96 + median);
+  } else {
+    return String.fromCharCode(96 + (median - 1) / 26) + String.fromCharCode(97 + (median - 1) % 26);
+  }
 }
 
 module.exports = median_to_letter;
