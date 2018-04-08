e1d94db09ca4b90910227ee06cceb47e627b31ef
liukxaa <644306737@qq.com>
2018-04-08 11:25:59 +0800

    完成 map_to_three_multiples

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,6 +1,6 @@
 'use strict';
-var map_to_three_multiples = function(collections){
-  return [3,9,15,12,27];
+let map_to_three_multiples = function(collections){
+  return collections.map(elem => elem * 3);
 };
 
 module.exports = map_to_three_multiples;
