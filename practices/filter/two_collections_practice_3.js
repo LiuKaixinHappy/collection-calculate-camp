5ea45c3ac0b45276a3f0ed36878566f208793ff1
liukxaa <644306737@qq.com>
2018-04-08 11:07:14 +0800

    完成 two_collections_practice_3

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,8 +1,11 @@
 'use strict';
 
-function choose_divisible_integer(collection_a, collection_b) {
+function divide_by(collection_b, elem) {
+  return collection_b.filter(x => elem % x === 0).length !== 0;
+}
 
-  //在这里写入代码
+function choose_divisible_integer(collection_a, collection_b) {
+  return collection_a.filter(elem => divide_by(collection_b, elem));
 }
 
 module.exports = choose_divisible_integer;
