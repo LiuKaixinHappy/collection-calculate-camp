f3bf6367e29599a6ab2fd01e15e9b1fbc79e0e92
liukxaa <644306737@qq.com>
2018-04-08 15:01:37 +0800

    完成 practice_4

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,11 @@
+function is_contains(collection_b, elem) {
+  return collection_b.filter(x => x === elem).length !== 0;
+}
+
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  let a = collection_a.map(elem => elem['key']);
+  let b = object_b['value'];
+  return a.filter(elem => is_contains(b, elem));
 }
 
 module.exports = collect_same_elements;
