61cf0858ada5bebeece20e5d827341682a1d5f4a
liukxaa <644306737@qq.com>
2018-04-08 17:09:11 +0800

    完成 calculate_average

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,7 @@
 'use strict';
-var calculate_average = function(collection){
-
+let calculate_average = function(collection){
+  let even_index_elem = collection.filter((elem, index) => index % 2 !== 0);
+  let sum = even_index_elem.reduce((total, current) => total + current);
+  return sum / even_index_elem.length;
 };
 module.exports = calculate_average;
