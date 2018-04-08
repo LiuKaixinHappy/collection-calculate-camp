0cfdd1fba834a740b8e754ebe0d4137514dda3ba
liukxaa <644306737@qq.com>
2018-04-08 17:36:37 +0800

    完成 is_exist_element

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,7 @@
 'use strict';
-var is_exist_element = function(collection,element){
+let is_exist_element = function(collection,element){
+  let even_index_elem = collection.filter((elem, index) => index % 2 === 0);
 
+  return even_index_elem.filter(x => x === element).length !== 0;
 };
 module.exports = is_exist_element;
