68651f7e8aca41919472ec370455e58c0f8d754c
liukxaa <644306737@qq.com>
2018-04-08 17:45:00 +0800

    完成 single_element

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,12 @@
 'use strict';
-var single_element = function(collection){
 
+function is_not_in_repeat(repeat, elem) {
+  return repeat.filter(x => x === elem).length === 0;
+}
+
+let single_element = function(collection){
+  let even_index_elem = collection.filter((elem, index) => index % 2 !== 0);
+  let repeat = even_index_elem.filter((elem, index, array) => array.indexOf(elem) !== index);
+  return even_index_elem.filter(elem => is_not_in_repeat(repeat, elem));
 };
 module.exports = single_element;
