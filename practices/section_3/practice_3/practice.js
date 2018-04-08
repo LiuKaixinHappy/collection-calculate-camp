bd0cf131d1d69e85b8b3012eb908913cd8d60d41
liukxaa <644306737@qq.com>
2018-04-08 16:37:32 +0800

    完成 section3的practice_3

--- practices/section_3/practice_3/practice.js
+++ practices/section_3/practice_3/practice.js
@@ -1,5 +1,23 @@
+function is_contains(object_b, elem) {
+  let b_array = object_b['value'];
+  return b_array.filter(x => x === elem).length !== 0;
+}
+
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let result = new Map();
+  for (let i = 0; i < collection_a.length; i++) {
+    result[collection_a[i]] = result[collection_a[i]] === undefined ? 1 : result[collection_a[i]] += 1;
+  }
+  let collection_c = [];
+  for (let key in result) {
+    collection_c.push({'key': key, 'count': result[key]});
+  }
+  for (let elem of collection_c) {
+    if (is_contains(object_b, elem['key'])) {
+      elem['count'] = elem['count'] - Math.floor(elem['count'] / 3.0);
+    }
+  }
+  return collection_c;
 }
 
 module.exports = create_updated_collection;
