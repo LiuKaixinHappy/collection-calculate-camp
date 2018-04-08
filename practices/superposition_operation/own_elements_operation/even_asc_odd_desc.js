7c19025616d2b5666ecd707c369bc1adacf89113
liukxaa <644306737@qq.com>
2018-04-08 17:49:20 +0800

    完成 even_asc_odd_desc

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,10 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
-
+let even_asc_odd_desc = function(collection){
+  let even_asc = collection.filter(elem => elem % 2 === 0).sort((x, y) => x - y);
+  let odd_des = collection.filter(elem => elem % 2 !== 0).sort((x, y) => y - x);
+  let result = [];
+  result.push(even_asc);
+  result.push(odd_des);
+  return [].concat.apply([], result);
 };
 module.exports = even_asc_odd_desc;
