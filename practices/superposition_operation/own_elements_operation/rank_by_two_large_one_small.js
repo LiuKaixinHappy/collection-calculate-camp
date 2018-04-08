6fb16dd0f043835fbf12fc432f235373aed5d9d2
liukxaa <644306737@qq.com>
2018-04-08 18:09:46 +0800

    完成 rank_by_two_large_one_small

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,15 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+
+function rank_by_two_large_one_small(collection) {
+  let asc_sort = collection.sort((x, y) => x - y);
+
+  for (let i = 0; i < asc_sort.length - 3; i+=3) {
+    let tmp = asc_sort[i];
+    asc_sort[i] = asc_sort[i + 1];
+    asc_sort[i + 1] = asc_sort[i + 2];
+    asc_sort[i + 2] = tmp;
+  }
+  return asc_sort;
 }
+
 module.exports = rank_by_two_large_one_small;
