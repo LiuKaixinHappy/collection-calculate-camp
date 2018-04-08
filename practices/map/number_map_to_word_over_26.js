eb38309a341accbe4c6a4e90dca7ac22c449851e
liukxaa <644306737@qq.com>
2018-04-08 11:32:52 +0800

    完成 number_map_to_word_over_26

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,14 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+
+function handle(elem) {
+  if (elem <= 26) {
+    return String.fromCharCode(96 + elem);
+  }
+  return String.fromCharCode(96 + (elem - 1) / 26) + String.fromCharCode(97 + (elem - 1) % 26);
+}
+
+let number_map_to_word_over_26 = function(collection){
+  return collection.map(elem => handle(elem));
 };
 
 module.exports = number_map_to_word_over_26;
