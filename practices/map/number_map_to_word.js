0ea6bca89bf2d446c5aab03933fbff8123ac0cf0
liukxaa <644306737@qq.com>
2018-04-08 11:28:02 +0800

    完成 number_map_to_word

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,6 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  return collection.map(elem => String.fromCharCode(96 + elem));
 };
 
 module.exports = number_map_to_word;
