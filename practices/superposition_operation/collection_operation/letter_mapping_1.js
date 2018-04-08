63572f7ea28cd7a850f32c83bcc62dac8597fa58
liukxaa <644306737@qq.com>
2018-04-08 16:44:14 +0800

    完成 letter_mapping_1

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function even_to_letter(collection) {
+  let even = collection.filter(elem => elem % 2 === 0);
 
-  //在这里写入代码
+  return even.map(elem => String.fromCharCode(96 + elem));
 }
 
 module.exports = even_to_letter;
