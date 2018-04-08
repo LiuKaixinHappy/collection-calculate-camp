d2715a460984f04569bbc76823a1248ae85d5232
liukxaa <644306737@qq.com>
2018-04-08 16:54:05 +0800

    完成 collection_operation的practice_3

--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  let odd = collection.filter(elem => elem % 2 !== 0);
+  let map_odd = odd.map(elem => elem * 3 + 5);
+  return map_odd.reduce((total, current) => total + current);
 }
 
 module.exports = hybrid_operation_to_uneven;
