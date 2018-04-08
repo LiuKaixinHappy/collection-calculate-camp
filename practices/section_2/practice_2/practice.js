59b9c72a20304277df69ef42267c46326d004268
liukxaa <644306737@qq.com>
2018-04-08 15:33:34 +0800

    完成 section2的practice_2

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,5 +1,18 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let result = new Map();
+  for (let i = 0; i < collection.length; i++) {
+    let tmp = collection[i].split('-');
+    if (tmp.length !== 1) {
+      result[tmp[0]] = parseInt(tmp[1], 10);
+    } else {
+      result[collection[i]] = result[collection[i]] === undefined ? 1 : result[collection[i]] += 1;
+    }
+  }
+  let new_res = [];
+  for (let key in result) {
+    new_res.push({'key': key, 'count': result[key]});
+  }
+  return new_res;
 }
 
 module.exports = count_same_elements;
