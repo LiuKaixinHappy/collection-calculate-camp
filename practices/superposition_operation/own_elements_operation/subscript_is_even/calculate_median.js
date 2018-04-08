f92ce8a3157c44d3f7db76ca867af022e0c15cd3
liukxaa <644306737@qq.com>
2018-04-08 17:21:57 +0800

    完成 calculate_median

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,10 @@
 'use strict';
-var calculate_median = function(collection){
-
+let calculate_median = function(collection){
+  let even_index_elem = collection.filter((elem, index) => index % 2 !== 0);
+  if (even_index_elem.length % 2 === 0) {
+    return (even_index_elem[even_index_elem.length / 2] + even_index_elem[even_index_elem.length / 2 - 1]) / 2;
+  } else {
+    return even_index_elem[(even_index_elem.length - 1) / 2] / 2;
+  }
 };
 module.exports = calculate_median;
