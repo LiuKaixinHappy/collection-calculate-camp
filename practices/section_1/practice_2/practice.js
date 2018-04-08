9508d49a6bdd4702bcad22d1c117368f600b5df1
liukxaa <644306737@qq.com>
2018-04-08 14:57:32 +0800

    完成 practice_2

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,5 +1,10 @@
+function is_contains(collection_b, elem) {
+  return collection_b.filter(x => x === elem).length !== 0;
+}
+
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  let flattern = [].concat.apply([], collection_b);
+  return collection_a.filter(elem => is_contains(flattern, elem));
 }
 
 module.exports = collect_same_elements;
